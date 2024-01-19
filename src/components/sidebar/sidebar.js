87f5b8f2beb3ec0aeb058be1cd65e14924dfe41d
Roman Lavery <romanlavery@Romans-MBP.lan>
2024-01-19 11:13:10 -0700

    began homepage format, created initial sidebar component

---
+++ src/components/sidebar/sidebar.js
@@ -0,0 +1,10 @@
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            sidebar
+        </div>
+    );
+}
+
+export default Sidebar;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,28 @@
-import { useState } from "react";
+
+import { Link } from "react-router-dom";
 function Home() {
     const [message, setMessage] = useState(false);
 
     return (
-        <div>
-            <h1>Home</h1>
-            {message && <p className="text-success">I've been clicked!</p>}
-            <button className="btn btn-primary" onClick={() => setMessage(!message)}>Click me</button>
+        <div className="home">
+            <div className="home-left">
+                <div className="home-header">
+                    <h1 className="home-header-title">Home</h1>
+                    <Link to='/' className="home-header-new-link">Create New Recipe</Link>
+                </div>
+
+                <div className="home-recipes">
+                    <h1 className="home-recipes-title">Recipes</h1>
+
+                    <div className="home-recipes-list">
+                        <Link to='/' className="home-recipes-list-item">Recipe 1</Link>
+                        <Link to='/' className="home-recipes-list-item">Recipe 2</Link>
+                        <Link to='/' className="home-recipes-list-item">Recipe 3</Link>
+                    </div>
+                </div>
+            </div>
+
+            {/* sidebar */}
         </div>
     );
 }
