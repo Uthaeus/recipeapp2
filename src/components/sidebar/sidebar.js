983af1d3717e2140f29f182416d883af564dc6dd
Roman Lavery <romanlavery@Romans-MBP.lan>
2024-01-19 17:05:52 -0700

    setting up initial sidebar layout

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -2,7 +2,25 @@
 function Sidebar() {
     return (
         <div className="sidebar">
-            sidebar
+            <h1 className="sidebar-title">Sort by Category</h1>
+
+            <ul className="sidebar-list">
+                <li className="sidebar-list-item">Category 1</li>
+                <li className="sidebar-list-item">Category 2</li>
+                <li className="sidebar-list-item">Category 3</li>
+                <li className="sidebar-list-item">Category 4</li>
+                <li className="sidebar-list-item">Category 5</li>
+            </ul>
+
+            <h1 className="sidebar-title">Sort by Main Ingredient</h1>
+
+            <ul className="sidebar-list">
+                <li className="sidebar-list-item">Main Ingredient 1</li>
+                <li className="sidebar-list-item">Main Ingredient 2</li>
+                <li className="sidebar-list-item">Main Ingredient 3</li>
+                <li className="sidebar-list-item">Main Ingredient 4</li>
+                <li className="sidebar-list-item">Main Ingredient 5</li>
+            </ul>
         </div>
     );
 }
